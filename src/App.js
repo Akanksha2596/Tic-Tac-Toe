564e8db6da5318d4eaac8a9963f6b6dc99c44f39
Akanksha Yadav <programmingwithyadavakanksha.40@gmail.com>
2022-10-18 00:39:53 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,140 @@
+import "./App.css";
+import { useEffect, useState } from "react";
+import Square from "./Componenst/Square";
+import { Winpattern } from "./Winpattern";
+
+function App() {
+  const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""]);
+  const [player, setPlayer] = useState("O");
+  const [result, setResult] = useState({ winner: "none", state: "none" });
+
+  useEffect(() => {
+    checkWin();
+    checkTie();
+    if (player === "X") {
+      setPlayer("O");
+    } else {
+      setPlayer("X");
+    }
+  }, [board]);
+
+  useEffect(() => {
+    if (result.state !== "none") {
+      alert(`Game Finished! And The Winning player is : ${result.winner}`);
+      restartGame();
+    }
+  }, [result]);
+
+  const chooseSquare = (square) => {
+    setBoard(
+      board.map((val, ind) => {
+        if (ind === square && val === "") {
+          return player;
+        }
+        return val;
+      })
+    );
+  };
+
+  const checkTie = () => {
+    let filled = true;
+    board.forEach((square) => {
+      if (square === "") {
+        filled = false;
+      }
+    });
+    if (filled) {
+      setResult({ winner: "No one ", state: "Tie" });
+    }
+  };
+
+  const restartGame = () => {
+    setBoard(["", "", "", "", "", "", "", "", ""]);
+    setPlayer("X");
+  };
+
+  const checkWin = () => {
+    Winpattern.forEach((currPattern) => {
+      const firstPlayer = board[currPattern[0]];
+      if (firstPlayer === "") return;
+      let foundWinningPattern = true;
+      currPattern.forEach((ind) => {
+        if (board[ind] !== firstPlayer) {
+          foundWinningPattern = false;
+        }
+      });
+
+      if (foundWinningPattern) {
+        setResult({ winner: player, state: "won" });
+      }
+    });
+  };
+  return (
+    <div className="App">
+      <div className="board">
+        <div className="row">
+          <Square
+            val={board[0]}
+            chooseSquare={() => {
+              chooseSquare(0);
+            }}
+          />
+          <Square
+            val={board[1]}
+            chooseSquare={() => {
+              chooseSquare(1);
+            }}
+          />
+          <Square
+            val={board[2]}
+            chooseSquare={() => {
+              chooseSquare(2);
+            }}
+          />
+        </div>
+        <div className="row">
+          <Square
+            val={board[3]}
+            chooseSquare={() => {
+              chooseSquare(3);
+            }}
+          />
+          <Square
+            val={board[4]}
+            chooseSquare={() => {
+              chooseSquare(4);
+            }}
+          />
+          <Square
+            val={board[5]}
+            chooseSquare={() => {
+              chooseSquare(5);
+            }}
+          />
+        </div>
+        <div className="row">
+          <Square
+            val={board[6]}
+            chooseSquare={() => {
+              chooseSquare(6);
+            }}
+          />
+          <Square
+            val={board[7]}
+            chooseSquare={() => {
+              chooseSquare(7);
+            }}
+          />
+          <Square
+            val={board[8]}
+            chooseSquare={() => {
+              chooseSquare(8);
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
